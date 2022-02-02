b534268b6078e93024852072644d38f07583e4f5
Adeel Shaukat <a.shaukat@ssidecisions.com>
2022-02-02 13:02:57 +0500

    creating validation pipe on DTO for restricting empty values of task status and description

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /**  
+  * We are telling NestJs that whenever you encounter validation decorator 
+  * e.g @IsNotEmpty() It will know to execute the validation pipes 
+  * */
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
 bootstrap();
