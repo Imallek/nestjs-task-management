f97323c663ac43ca526fa35ed50c2c4650a28e36
Adeel Shaukat <a.shaukat@ssidecisions.com>
2022-02-02 14:44:52 +0500

    proper validation on search filters

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,19 @@
+import { IsEnum, IsNotEmpty, IsOptional } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 export class GetTasksFilterDto {
-    status?:TaskStatus;
-    search?:string;
+
+    /* 
+    We have to provide IsOptional decorator (although we have mentioned it in the TS).
+    Reason is that TS doesn't exit post-compilation and for validation we have to 
+    explicitly mentioned that field is optional and then apply the remaining validation rules
+     */
+
+    @IsOptional()
+    @IsEnum(TaskStatus)
+    status?: TaskStatus;
+
+    @IsOptional()
+    @IsNotEmpty()
+    search?: string;
 }
