b05a9ea7b5c77da9ec95b4b7e1da770498e4f1be
Adeel Shaukat <a.shaukat@ssidecisions.com>
2022-02-02 15:20:56 +0500

    defining a task repository

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from './tasks.controller';
+import { TaskRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([TaskRepository])], // This line allows to dependency inject TaskRepository wherever we want in the modlule
   controllers: [TasksController],
   providers: [TasksService]
 })
