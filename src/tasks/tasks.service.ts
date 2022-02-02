cf047751ffa4d86386a4feb81a3014e391c438fc
Adeel Shaukat <a.shaukat@ssidecisions.com>
2022-02-02 13:14:52 +0500

    Error handling for deleting a non-existing task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -47,6 +47,7 @@ export class TasksService {
     }
 
     deleteTask(id:string):void{
+        const found = this.getTaskById(id);
         this.tasks = this.tasks.filter(task => task.id != id)
     }
 
