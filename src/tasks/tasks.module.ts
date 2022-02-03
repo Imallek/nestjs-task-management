8dbd697cde75f327d87ae10a383c1c8e7697f18e
Adeel Shaukat <a.shaukat@ssidecisions.com>
2022-02-03 17:50:39 +0500

    guarding the Tasks Routes

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { TasksController } from './tasks.controller';
 import { TaskRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskRepository])], // This line allows to dependency inject TaskRepository wherever we want in the modlule
+  imports: [TypeOrmModule.forFeature([TaskRepository]), AuthModule], // This line allows to dependency inject TaskRepository wherever we want in the modlule
   controllers: [TasksController],
   providers: [TasksService],
 })
