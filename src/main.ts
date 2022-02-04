3c5d31f8b8f446708081a2485027b1644f351ff9
Adeel Shaukat <a.shaukat@ssidecisions.com>
2022-02-04 11:52:03 +0500

    serialize User Data

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './tranform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
    * e.g @IsNotEmpty() It will know to execute the validation pipes
    * */
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
 bootstrap();
